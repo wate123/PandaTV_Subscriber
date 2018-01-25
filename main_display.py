17c8a6908ff6943e96c5b1867309c814db6906b1
ziz19 <ziz19@pitt.edu>
2018-01-24 21:21:46 -0500

    Changing size,sound module (#10)
    * chaning size
    
    * converting music format
    
    * modify window size
    
    * chanding to a different sound module
    
    使用mp3文件

--- main_display.py
+++ main_display.py
@@ -2,7 +2,7 @@ from tkinter import *
 from crawl import Crawl
 import threading
 import time
-import winsound
+from pygame import mixer
 
 class MainDisplay:
 
@@ -15,41 +15,43 @@ class MainDisplay:
         self.c = Crawl(goal)      # 初始化Crawler
         # 设置GUI界面
         self.root = Tk()
-
+        mixer.init()
+        mixer.music.load('doorbell.mp3')
         ###########################     设置初始windows位置 ##################
-        self.root.geometry('220x55+40+560')         # 长 X  宽  + 向右平移 + 向下平移
+        self.root.geometry('340x37+21+733')         # 长 X  宽  + 向右平移 + 向下平移
         #####################################################################
 
         self.root.title('就是要莽')
-        top_frame = Frame(self.root)  # 上层frame用于显示信息
-        top_frame.pack(fill=BOTH)
+        left_frame = Frame(self.root)  # 左边frame用于显示信息
+        left_frame.grid(row=0,column=0)
         self.label_text1 = StringVar()
         self.label_text1.set('今日订阅:')
-        text_label = Label(top_frame, textvariable=self.label_text1,font="32")
+        text_label = Label(left_frame, textvariable=self.label_text1,font="32")
         text_label.grid(row=0,sticky='w')
         self.cur_num = StringVar()   # 当前订阅数
-        num_label = Label(top_frame, textvariable=self.cur_num,fg="red",font="28")
+        num_label = Label(left_frame, textvariable=self.cur_num,fg="red",font="28")
         num_label.grid(row=0, column=1,sticky='e')
         self.label_text2 = StringVar()
         self.label_text2.set('/'+str(self.goal))
-        objective_label = Label(top_frame,textvariable=self.label_text2,font="28")
+        objective_label = Label(left_frame,textvariable=self.label_text2,font="28")
         objective_label.grid(row=0,column=2,sticky='w')
-        top_frame.columnconfigure(0,weight=4)     # 调整widget位置
-        top_frame.columnconfigure(1,weight=2)
-        top_frame.columnconfigure(2,weight=2)
+        # left_frame.columnconfigure(0,weight=4)     # 调整widget位置
+        # left_frame.columnconfigure(1,weight=2)
+        # left_frame.columnconfigure(2,weight=2)
 
-        bottom_frame = Frame(self.root)  # 下层frame用于手动获取最新订阅量
-        bottom_frame.pack(fill=BOTH, side=BOTTOM)
-        refresh_button = Button(bottom_frame, text='手动刷新',font="25")
+        right_frame = Frame(self.root)  # 右边frame用于手动获取最新订阅量和当前订阅人数
+        right_frame.grid(row=0,column=1)
+        # bottom_frame.pack(fill=BOTH, side=BOTTOM)
+        refresh_button = Button(right_frame, text='手动刷新',font="25")
         refresh_button.bind('<Button-1>', self.refresh)
         refresh_button.grid(row=0,column=0,sticky=("N", "S", "E", "W"),padx=4,pady=4)
-        fans_button=Button(bottom_frame,text='当前订阅',font="25")
+        fans_button=Button(right_frame,text='当前订阅',font="25")
         fans_button.bind('<Button-1>', self.refresh_total_fans)
         fans_button.grid(row=0,column=1,sticky=("N", "S", "E", "W"),padx=4,pady=4)
-        bottom_frame.columnconfigure(0,weight=1)
-        bottom_frame.columnconfigure(1,weight=1)
-        self.root.rowconfigure(0,weight=3)   # 调整widget位置
-        self.root.rowconfigure(1,weight=1)
+        right_frame.columnconfigure(0,weight=1)
+        right_frame.columnconfigure(1,weight=1)
+        self.root.columnconfigure(0,minsize=50)
+        self.root.columnconfigure(1,weight=1)   # 调整widget位置
 
         t = threading.Thread(target=self.start_crawl)   # 开始运行
         t.daemon = True
@@ -67,7 +69,7 @@ class MainDisplay:
 
 
     def play_sound(self):
-        winsound.PlaySound('doorbell.wav',winsound.SND_FILENAME)
+        mixer.music.play()
 
     def refresh(self,event):
         t = threading.Thread(target=self.print_fans)
