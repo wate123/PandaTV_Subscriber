839e5994edadd06e11c6a8a50be77c07e4cce43c
Zinan Zhuang <ziz19@pitt.edu>
2018-01-29 10:43:26 -0500

    background has color

--- main_display.py
+++ main_display.py
@@ -17,40 +17,41 @@ class MainDisplay:
         self.c = Crawl(goal,str(room_id))      # 初始化Crawler
         # 设置GUI界面
         self.root = Tk()
+        self.root.configure(background='#f3b513')
         if os.path.isfile('doorbell.mp3'):      # 在载入音乐前检查音乐是否存在
             mixer.init()
             mixer.music.load('doorbell.mp3')
             self.has_music = True
         else:   self.has_music = False
         ###########################     设置初始windows位置 ##################
-        self.root.geometry('340x37+21+733')         # 长 X  宽  + 向右平移 + 向下平移
+        self.root.geometry('200x37+21+733')         # 长 X  宽  + 向右平移 + 向下平移
         #####################################################################
 
-        self.root.title('就是要莽')
-        left_frame = Frame(self.root)  # 左边frame用于显示信息
+        self.root.title('太阳')
+        left_frame = Frame(self.root,background='#f3b513')  # 左边frame用于显示信息
         left_frame.grid(row=0,column=0)
         self.label_text1 = StringVar()
         self.label_text1.set('今日订阅:')
-        text_label = Label(left_frame, textvariable=self.label_text1,font="32")
+        text_label = Label(left_frame, textvariable=self.label_text1,font="32",background='#f3b513')
         text_label.grid(row=0,sticky='w')
         self.cur_num = StringVar()   # 当前订阅数
-        num_label = Label(left_frame, textvariable=self.cur_num,fg="red",font="28")
+        num_label = Label(left_frame, textvariable=self.cur_num,fg="red",font="28",background='#f3b513')
         num_label.grid(row=0, column=1,sticky='e')
         self.label_text2 = StringVar()
         self.label_text2.set('/'+str(self.goal))
-        objective_label = Label(left_frame,textvariable=self.label_text2,font="28")
+        objective_label = Label(left_frame,textvariable=self.label_text2,font="28",background='#f3b513')
         objective_label.grid(row=0,column=2,sticky='w')
         # left_frame.columnconfigure(0,weight=4)     # 调整widget位置
         # left_frame.columnconfigure(1,weight=2)
         # left_frame.columnconfigure(2,weight=2)
 
-        right_frame = Frame(self.root)  # 右边frame用于手动获取最新订阅量和当前订阅人数
+        right_frame = Frame(self.root,background='#f3b513')  # 右边frame用于手动获取最新订阅量和当前订阅人数
         right_frame.grid(row=0,column=1)
         # bottom_frame.pack(fill=BOTH, side=BOTTOM)
-        refresh_button = Button(right_frame, text='手动刷新',font="25")
+        refresh_button = Button(right_frame, text='刷新',font="25",background='#f3b513')
         refresh_button.bind('<Button-1>', self.refresh)
         refresh_button.grid(row=0,column=0,sticky=("N", "S", "E", "W"),padx=4,pady=4)
-        fans_button=Button(right_frame,text='当前订阅',font="25")
+        fans_button=Button(right_frame,text='总订',font="25",background='#f3b513')
         fans_button.bind('<Button-1>', self.refresh_total_fans)
         fans_button.grid(row=0,column=1,sticky=("N", "S", "E", "W"),padx=4,pady=4)
         right_frame.columnconfigure(0,weight=1)
